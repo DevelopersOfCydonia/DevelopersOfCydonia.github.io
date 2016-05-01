a6e2ef2d0506257fdbc36c68023214207f2682eb
Matteo Lobello <matteolob1704@gmail.com>
2016-05-01 21:50:27 +0200

    Fix JS indentation

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
 function getRandom() {
-			return Math.floor(Math.random() * 255);
-		}
+	return Math.floor(Math.random() * 255);
+}
 
-		function buildBackgroundString(matteo) {
-			return "linear-gradient(rgba(" + getRandom() + ", " + getRandom() + ", " + getRandom() + ", 0.45), rgba(" + getRandom() + ", " + getRandom() + ", " + getRandom() + ", 0.45)), url(img/" + (matteo ? "matteo.jpeg" : "eliseo.jpg") + ") no-repeat;";
-		}
+function buildBackgroundString(matteo) {
+	return "linear-gradient(rgba(" + getRandom() + ", " + getRandom() + ", " + getRandom() + ", 0.45), rgba(" + getRandom() + ", " + getRandom() + ", " + getRandom() + ", 0.45)), url(img/" + (matteo ? "matteo.jpeg" : "eliseo.jpg") + ") no-repeat;";
+}
 
-		document.getElementById("matteo").style = "background: " + buildBackgroundString(true) + "background-size: cover; background-clip: border-box; background-position: center;";
-		document.getElementById("eliseo").style = "background: " + buildBackgroundString(false) + "background-size: cover; background-clip: border-box; background-position: center;";
+document.getElementById("matteo").style = "background: " + buildBackgroundString(true) + "background-size: cover; background-clip: border-box; background-position: center;";
+document.getElementById("eliseo").style = "background: " + buildBackgroundString(false) + "background-size: cover; background-clip: border-box; background-position: center;";
